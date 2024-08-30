93c981d19893fdd207effd2fabfb49301bc40e03
Adam Pawełek <adam.pwk@outlook.com>
2024-08-30 20:04:51 +0200

    fixed translator.py

--- llm_translate/translator.py
+++ llm_translate/translator.py
@@ -27,10 +27,10 @@ class Translator(ABC):
         number_of_languages: int
 
     class TextLanguage(BaseModel):
-        ISO_639_1_code: str | None
-        ISO_639_2_code: str  | None
-        ISO_639_3_code: str  | None
-        language_name: str   | None
+        ISO_639_1_code: str
+        ISO_639_2_code: str
+        ISO_639_3_code: str
+        language_name: str
 
     supported_languages: None
     def __init__(self):
@@ -71,13 +71,7 @@ class Translator(ABC):
 
             )
         except Exception as e:
-            detected_language = Translator.TextLanguage(
-                ISO_639_1_code=None,
-                ISO_639_2_code=None,
-                ISO_639_3_code=None,
-                language_name=None,
-
-            )
+            detected_language = None
         return detected_language
 
     def get_text_language(self, text: str) -> TextLanguage:
