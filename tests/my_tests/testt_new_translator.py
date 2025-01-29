0ece635ba3a3c8aa56ca9e75d5a8cd5d9571212d
Adam <adam.pwk@outlook.com>
2025-01-29 18:43:06 +0100

    added description

--- tests/my_tests/testt_new_translator.py
+++ tests/my_tests/testt_new_translator.py
@@ -4,7 +4,13 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(model_name="gpt-4o", openai_api_key=os.getenv("OPENAI_API_KEY"))
 translator = Translator(llm=llm, max_length_text_chunk_to_translate=100, max_length_text_chunk_to_translate_multiple_languages=100)
-print(translator.get_text_language("Hi how are you?"))
+text_language = translator.get_text_language("Hi how are you?")
+if text_language:
+    print(text_language.ISO_639_1_code)
+    print(text_language.ISO_639_2_code)
+    print(text_language.ISO_639_3_code)
+    print(text_language.language_name)
+
 print(translator.translate("Hi how are you?", "Spanish"))
 
 
